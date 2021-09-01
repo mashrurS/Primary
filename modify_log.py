0183b2d040ef089737a5134436cc1a74c2817137
mashrurS <mashrur.shuvo@gmail.com>
2021-09-01 14:44:40 -0400

    Update modify_log.py

--- modify_log.py
+++ modify_log.py
@@ -4,7 +4,7 @@ def modify_log():
 	logger = logging.getLogger(__name__)
 	logger.setLevel(logging.INFO)
 	formatter = logging.Formatter('%(asctime)s:%(levelname)s : %(name)s : %(message)s')
-	file_handler = logging.FileHandler('D:/MKTCloudFeeds/log/extract_to_csv.log')
+	file_handler = logging.FileHandler('xxxx/log/extract_to_csv.log')
 	file_handler.setFormatter(formatter)
 
 	if (logger.hasHandlers()):
