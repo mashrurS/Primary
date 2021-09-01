b081481bfa9e16cfb472e8fb218b665a9cf4d2f1
mashrurS <mashrur.shuvo@gmail.com>
2021-09-01 14:45:10 -0400

    Update retry.py

--- retry.py
+++ retry.py
@@ -5,7 +5,7 @@ import traceback
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s:%(levelname)s : %(name)s : %(message)s')
-file_handler = logging.FileHandler('//CORP-ETLDB01/MKTCloudFeeds/log/extract_to_csv.log')
+file_handler = logging.FileHandler('xxxxx/extract_to_csv.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
@@ -14,6 +14,6 @@ try:
 except Exception as e:
 	logger.exception(e)
 	
-	send_mail(	to_address= 'sqladmins@cigarsinternational.com', 
+	send_mail(	to_address= 'xxxx@xxx.com', 
 				subject='ALERT: Marketing Cloud Data Feed - Execution Error' , 
     			email_message = traceback.format_exc())
